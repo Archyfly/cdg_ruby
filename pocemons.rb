8587904efa93bb393621db2a6b8fcfb1b9c17587
Archyfly <flyaway400@gmail.com>
2021-07-09 23:11:54 +0800

    Add solution for print pocemons in same format

--- pocemons.rb
+++ pocemons.rb
@@ -3,5 +3,28 @@
 # Выводит в конце массив, содержащий хэши покемонов в формате 
 # [{name: “Vasya“, color: “Yellow“}, {name: “Oleg“, color: “Red“}]
 
+def pocemon
+  puts "Сколько покемонов вы хотите добавить?"
+  n = gets.chomp.to_i
+  i = 1;
+  pocehash = {}
+  @pocemon_mass = []
+  while (i<=n) do
+    puts "Введите имя #{i} покемона"
+    name = gets.chomp.to_s
+    puts "Введите цвет покемона"
+    color = gets.chomp.to_s
+    pocehash = {name: name, color: color}
+    add_pocemon(pocehash)
+    i = i + 1
+  end
+  print @pocemon_mass
+end
+
+def add_pocemon(pocehash)
+  @pocemon_mass.push(pocehash)
+end
+
+print pocemon
 
 
