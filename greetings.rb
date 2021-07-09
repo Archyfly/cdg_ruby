61ae562808d5439fdce842c789d7d721e4c3f812
Archyfly <flyaway400@gmail.com>
2021-07-09 22:25:48 +0800

    Add solution to task 1. Greetings

--- greetings.rb
+++ greetings.rb
@@ -3,4 +3,21 @@
 # <18: Привет, #{name}. Тебе меньше 18 лет, но начать учиться программировать # никогда не рано"
 # ELSE "Привет, #{name}. Самое время занятся делом"
 
+def greets(name, age)
+  if age < 18
+    return "Привет, #{name}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+  else
+    return "Привет, #{name}. Самое время занятся делом"
+  end
+end
+
+def enter_your_params
+  puts "Введи своё имя и фамилию:"
+  your_name = gets.chomp.to_s
+  puts "Сколько тебе лет?"
+  your_age = gets.chomp.to_i
+  puts greets(your_name, your_age)
+end
+
+enter_your_params
 
