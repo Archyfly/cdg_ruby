52cf5a41667b25e1bcaa756d0ca6c4133200fc30
Mikhail Kashinin <flyaway400@gmail.com>
2021-07-13 15:57:45 +0800

    add check: input age, file exist

--- reading_file.rb
+++ reading_file.rb
@@ -7,6 +7,7 @@
 перезапросить ввод. Программа завершается выводом на экран содержимого файла results.txt построчно, если все
 студенты из первого файла были записаны во второй или если пользователь ввел с клавиатуры -1
 =end
+AGES = (17..99).freeze
 
 def compare_files
   return File.read("students.txt").size == File.read("results.txt").size
@@ -16,27 +17,24 @@ def read_file(str, age)
   file = File.open(str)
   file_data = file.read.split("\n")
   arr_to_insert = []
+  count = 0
   file_data.each do |line|
     if line.include?(age)
       arr_to_insert.push(line)
+      count = count + 1
     end
   end
+  puts "Записей добавлено: #{count}"
   arr_to_insert.each do |line|
-    wts = line + "\n"
-    write_to_results(wts)
+    string_for_write = line + "\n"
+    write_to_results(string_for_write)
   end
   file.close
 end
 
-def check_input(input)
-
-end
-
-def write_to_results(wts)
+def write_to_results(string_for_write)
   file = File.open("results.txt", "a+")
-  file.write(wts)
-  puts "Запись добавлена"
-  puts compare_files
+  file.write(string_for_write)
   file.close
 end
 
@@ -47,22 +45,29 @@ end
 
 def view_menu
   input_age = []
+  if File.exist?("results.txt")
+    file = File.open("results.txt", "w")
+    file.close
+  else
+    File.new("results.txt")
+  end
   loop do
     break if compare_files == true
     list
-    #file_for_search = "students.txt"
-    #file_for_write =  "results.txt"
     choice = gets.chomp
     break if choice == '-1'
     if input_age.include?(choice) || choice == ""
       puts "Пустой ввод или выбор по этому году уже проводился."
+      next
+    elsif !(AGES.include?(choice.to_i))
+      puts "Некорректный ввод. Введите значение от 17 до 99"
       next
     else
       read_file('students.txt', choice)
     end
     input_age.push(choice)
   end
-  puts "Из предоставленного списка перенесены все записи."
+  puts "Из предоставленного списка перенесены записи, соответствующие возрастам #{input_age}."
   file = File.open("results.txt")
   file_data = file.readlines
   puts file_data
